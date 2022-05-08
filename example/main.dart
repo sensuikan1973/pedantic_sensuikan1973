85c5b5110db26fee1940f6b36ee70df0b3b96fa7
sensuikan1973 <sensuikan1973@gmail.com>
2022-05-09 03:27:37 +0900

    fix typo

--- example/main.dart
+++ example/main.dart
@@ -2,5 +2,5 @@ import 'dart:io';
 
 void main() {
   stdout.writeln('This line is more than 80 chars, but analyzer do not warn. lines_longer_than_80_chars is disabled.');
-  if (''.isEmpty) stdout.writeln('analyzer do not warn. always_put_control_body_on_new_line is diabled.');
+  if (''.isEmpty) stdout.writeln('analyzer do not warn. always_put_control_body_on_new_line is disabled.');
 }
