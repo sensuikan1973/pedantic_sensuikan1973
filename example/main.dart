e928bbf777e1bc95034937619829f892e1ab2a63
sensuikan1973 <23427957+sensuikan1973@users.noreply.github.com>
2025-02-13 07:52:23 +0900

    bump Dart sdk min minor version (#129)
    * bump Dart SDK min minor version
    
    * fmt

--- example/main.dart
+++ example/main.dart
@@ -1,6 +1,12 @@
 import 'dart:io';
 
 void main() {
-  stdout.writeln('This line is more than 80 chars, but analyzer do not warn. lines_longer_than_80_chars is disabled.');
-  if (''.isEmpty) stdout.writeln('analyzer do not warn. always_put_control_body_on_new_line is disabled.');
+  stdout.writeln(
+    'This line is more than 80 chars, but analyzer do not warn. lines_longer_than_80_chars is disabled.',
+  );
+  if (''.isEmpty) {
+    stdout.writeln(
+      'analyzer do not warn. always_put_control_body_on_new_line is disabled.',
+    );
+  }
 }
